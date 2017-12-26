317aad36be20146e252bbb9caf5742eeef37d6aa
zjun <565061763@qq.com>
2017-12-26 16:32:57 +0800

    重构

--- app/src/main/java/com/fanwe/utils_extend/MainActivity.java
+++ app/src/main/java/com/fanwe/utils_extend/MainActivity.java
@@ -2,11 +2,15 @@ package com.fanwe.utils_extend;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.fanwe.lib.utils.extend.bitmap.FBitmapCompressor;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity
 {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     private FBitmapCompressor mBitmapCompressor;
 
@@ -17,5 +21,11 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         mBitmapCompressor = new FBitmapCompressor(this);
+
+        if (mBitmapCompressor.decodeFile("/sdcard/test.png"))
+        {
+            File file = mBitmapCompressor.saveToFile();
+            Log.i(TAG, file.getAbsolutePath());
+        }
     }
 }

ee0fdaba59117b2926c1e684d61eead2f0597989
zjun <565061763@qq.com>
2017-12-26 16:16:58 +0800

    重构

--- lib/src/main/java/com/fanwe/lib/utils/extend/bitmap/FBitmapCompressor.java
+++ lib/src/main/java/com/fanwe/lib/utils/extend/bitmap/FBitmapCompressor.java
@@ -3,28 +3,66 @@ package com.fanwe.lib.utils.extend.bitmap;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.DisplayMetrics;
+
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 /**
  * Created by Administrator on 2017/12/26.
  */
 public class FBitmapCompressor
 {
-    private int mMaxWidth;
+    public static final String COMPRESSED_FILE_DIR_NAME = "compressed_image";
+
     private int mMaxFileSize = 1024 * 1024;
+    private int mMaxWidth;
+    private int mMaxHeight;
 
     private Bitmap mBitmap;
 
-    private FBitmapCompressor(Context context)
+    private Context mContext;
+    private File mCompressedFileDir;
+
+    private Exception mException;
+
+    public FBitmapCompressor(Context context)
     {
-        mMaxWidth = context.getResources().getDisplayMetrics().widthPixels;
+        if (context == null)
+        {
+            throw new NullPointerException("context must not be null");
+        }
+        mContext = context.getApplicationContext();
+
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        mMaxWidth = displayMetrics.widthPixels;
+        mMaxHeight = displayMetrics.heightPixels;
+    }
+
+    public File getCompressedFileDir()
+    {
+        if (mCompressedFileDir == null)
+        {
+            mCompressedFileDir = new File(mContext.getExternalCacheDir(), COMPRESSED_FILE_DIR_NAME);
+        }
+        if (!mCompressedFileDir.exists())
+        {
+            mCompressedFileDir.mkdirs();
+        }
+        return mCompressedFileDir;
     }
 
     public void setMaxWidth(int maxWidth)
     {
-        if (maxWidth > 0)
-        {
-            mMaxWidth = maxWidth;
-        }
+        mMaxWidth = maxWidth;
+    }
+
+    public void setMaxHeight(int maxHeight)
+    {
+        mMaxHeight = maxHeight;
     }
 
     public void setMaxFileSize(int maxFileSize)
@@ -44,26 +82,67 @@ public class FBitmapCompressor
         mBitmap = bitmap;
     }
 
-    public FBitmapCompressor decodeFile(String filePath)
+    public boolean decodeFile(String filePath)
     {
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
         BitmapFactory.decodeFile(filePath, options);
 
-        int maxHeight = getScaleHeight(options.outWidth, options.outHeight, mMaxWidth);
-
-        options.inSampleSize = calculateInSampleSize(options, mMaxWidth, maxHeight);
+        options.inSampleSize = calculateInSampleSize(options, mMaxWidth, mMaxHeight);
         options.inJustDecodeBounds = false;
 
         try
         {
-            Bitmap bitmap = scaleBitmapIfNeed(BitmapFactory.decodeFile(filePath, options),
-                    mMaxWidth, maxHeight, true);
-            saveBitmap(bitmap);
-        } catch (Throwable e)
+            Bitmap bitmap = BitmapFactory.decodeFile(filePath, options);
+            Bitmap bitmapScaled = scaleBitmapIfNeed(bitmap, mMaxWidth, mMaxHeight);
+            if (bitmapScaled != bitmap)
+            {
+                bitmap.recycle();
+            }
+
+            Bitmap bitmapCompressed = compressBitmapToFileSize(bitmapScaled, mMaxFileSize, 5);
+            if (bitmapCompressed != null)
+            {
+                if (bitmapCompressed != bitmapScaled)
+                {
+                    bitmapScaled.recycle();
+                }
+                saveBitmap(bitmapCompressed);
+                return true;
+            } else
+            {
+                return false;
+            }
+        } catch (Exception e)
         {
+            mException = e;
+            return false;
         }
-        return this;
+    }
+
+    public File saveToFile()
+    {
+        if (mBitmap == null)
+        {
+            return null;
+        }
+        File file = newFileUnderDir(getCompressedFileDir(), ".jpg");
+        FileOutputStream fos = null;
+        try
+        {
+            fos = new FileOutputStream(file);
+            mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            mBitmap.recycle();
+            mBitmap = null;
+            return file;
+        } catch (FileNotFoundException e)
+        {
+            mException = e;
+            return null;
+        } finally
+        {
+            closeQuietly(fos);
+        }
     }
 
     public Bitmap getBitmap()
@@ -71,11 +150,68 @@ public class FBitmapCompressor
         return mBitmap;
     }
 
+    public Exception getException()
+    {
+        return mException;
+    }
+
+    /**
+     * 压缩bitmap到不超过指定的大小(文件大小，非bitmap大小)
+     *
+     * @param bitmap
+     * @param maxSize      指定的最大值
+     * @param deltaQuality 当前quality不满足的时候，quality递减的大小
+     * @return
+     */
+    private Bitmap compressBitmapToFileSize(Bitmap bitmap, long maxSize, int deltaQuality)
+    {
+        if (maxSize <= 0)
+        {
+            return bitmap;
+        }
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try
+        {
+            int quality = 100;
+            while (true)
+            {
+                if (quality <= 1)
+                {
+                    break;
+                }
+                baos.reset();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, quality, baos);
+
+                if (baos.size() > maxSize)
+                {
+                    quality -= deltaQuality;
+                    continue;
+                } else
+                {
+                    // 压缩到指定大小成功
+                    byte[] arrByte = baos.toByteArray();
+                    return BitmapFactory.decodeByteArray(arrByte, 0, arrByte.length);
+                }
+            }
+        } finally
+        {
+            closeQuietly(baos);
+        }
+        return null;
+    }
+
     protected int calculateInSampleSize(BitmapFactory.Options options, int maxWidth, int maxHeight)
     {
+        if (maxWidth <= 0 || maxHeight <= 0)
+        {
+            return 1;
+        }
+
+        int inSampleSize = 1;
+
         final int height = options.outHeight;
         final int width = options.outWidth;
-        int inSampleSize = 1;
 
         if (width > maxWidth || height > maxHeight)
         {
@@ -110,28 +246,61 @@ public class FBitmapCompressor
         return inSampleSize;
     }
 
-    private static int getScaleHeight(int scaleWidth, int scaleHeight, int width)
+    /**
+     * 如果bitmap的宽或者高超过设置的最大宽或者高，则进行压缩
+     *
+     * @param bitmap
+     * @param maxWidth  最大宽度
+     * @param maxHeight 最大高度
+     * @return
+     */
+    private static Bitmap scaleBitmapIfNeed(Bitmap bitmap, int maxWidth, int maxHeight)
     {
-        if (scaleWidth == 0)
+        if (maxWidth <= 0 || maxHeight <= 0)
         {
-            return 0;
+            return bitmap;
         }
-        return scaleHeight * width / scaleWidth;
-    }
 
-    private static Bitmap scaleBitmapIfNeed(Bitmap bitmap, int targetWidth, int targetHeight, boolean recycle)
-    {
-        if (bitmap.getWidth() > targetHeight || bitmap.getHeight() > targetHeight)
+        if (bitmap.getWidth() > maxHeight || bitmap.getHeight() > maxHeight)
         {
-            Bitmap result = Bitmap.createScaledBitmap(bitmap, targetWidth, targetHeight, true);
-            if (recycle)
-            {
-                bitmap.recycle();
-            }
-            return result;
+            return Bitmap.createScaledBitmap(bitmap, maxWidth, maxHeight, true);
         } else
         {
             return bitmap;
+        }
+    }
+
+    private static File newFileUnderDir(File dir, String ext)
+    {
+        if (dir == null)
+        {
+            return null;
+        }
+        if (ext == null)
+        {
+            ext = "";
+        }
+
+        long current = System.currentTimeMillis();
+        File file = new File(dir, String.valueOf(current + ext));
+        while (file.exists())
+        {
+            current++;
+            file = new File(dir, String.valueOf(current + ext));
+        }
+        return file;
+    }
+
+    private static void closeQuietly(Closeable closeable)
+    {
+        if (closeable != null)
+        {
+            try
+            {
+                closeable.close();
+            } catch (Throwable ignored)
+            {
+            }
         }
     }
 }

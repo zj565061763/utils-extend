2329b5187f492ac783089241832cddf9a53f714f
zjun <565061763@qq.com>
2017-12-29 14:42:14 +0800

    重构

--- app/src/main/java/com/fanwe/utils_extend/MainActivity.java
+++ app/src/main/java/com/fanwe/utils_extend/MainActivity.java
@@ -1,10 +1,14 @@
 package com.fanwe.utils_extend;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.fanwe.lib.utils.extend.FImageCompressor;
 import com.fanwe.lib.utils.extend.FImageGetter;
 
 import java.io.File;
@@ -13,15 +17,29 @@ public class MainActivity extends AppCompatActivity
 {
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    private ImageView iv_image;
+
     private FImageGetter mImageGetter;
+    private FImageCompressor mImageCompressor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv_image = findViewById(R.id.iv_image);
 
+        testImageCompressor();
         testImageGetter();
+
+        iv_image.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                mImageGetter.getImageFromAlbum();
+            }
+        });
     }
 
     private void testImageGetter()
@@ -33,6 +51,9 @@ public class MainActivity extends AppCompatActivity
             public void onResultFromAlbum(File file)
             {
                 Log.i(TAG, "onResultFromAlbum:" + file.getAbsolutePath());
+
+                Bitmap bitmap = mImageCompressor.compressFileToBitmap(file.getAbsolutePath());
+                iv_image.setImageBitmap(bitmap);
             }
 
             @Override
@@ -47,7 +68,11 @@ public class MainActivity extends AppCompatActivity
                 Log.e(TAG, desc);
             }
         });
-        mImageGetter.getImageFromAlbum();
+    }
+
+    private void testImageCompressor()
+    {
+        mImageCompressor = new FImageCompressor(this);
     }
 
     @Override

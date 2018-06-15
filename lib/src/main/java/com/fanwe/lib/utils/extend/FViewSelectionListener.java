cf00c035e7cfebe34e2397a34c0936426838c622
zjun <565061763@qq.com>
2018-06-15 10:03:47 +0800

    FViewSelectionListener

---
+++ lib/src/main/java/com/fanwe/lib/utils/extend/FViewSelectionListener.java
@@ -0,0 +1,65 @@
+package com.fanwe.lib.utils.extend;
+
+import android.view.View;
+
+/**
+ * view的选中状态变化监听
+ *
+ * @param <T>
+ */
+public abstract class FViewSelectionListener<T extends View>
+{
+    private boolean mSelected;
+
+    public final T getView()
+    {
+        return (T) mOnPreDrawListener.getView();
+    }
+
+    public final void setView(T view)
+    {
+        mOnPreDrawListener.setView(view);
+        mOnPreDrawListener.register();
+    }
+
+    private final FOnPreDrawListener mOnPreDrawListener = new FOnPreDrawListener()
+    {
+        @Override
+        protected void onStateChanged(boolean register)
+        {
+            super.onStateChanged(register);
+            if (register)
+                notifyStateIfNeed();
+        }
+
+        @Override
+        public boolean onPreDraw()
+        {
+            notifyStateIfNeed();
+            return true;
+        }
+    };
+
+    private void notifyStateIfNeed()
+    {
+        final T view = getView();
+        if (view == null)
+            return;
+
+        final boolean selected = view.isSelected();
+        if (mSelected != selected)
+        {
+            mSelected = selected;
+            view.post(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    onSelectionStateChanged(selected, view);
+                }
+            });
+        }
+    }
+
+    protected abstract void onSelectionStateChanged(boolean selected, T view);
+}

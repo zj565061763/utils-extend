19e168d1db2c40055b87405e110fda8855429e0f
zjun <565061763@qq.com>
2019-01-16 20:55:44 +0800

    demo

--- app/src/main/java/com/sd/utils_extend/MainActivity.java
+++ app/src/main/java/com/sd/utils_extend/MainActivity.java
@@ -2,7 +2,10 @@ package com.sd.utils_extend;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+
+import com.sd.lib.utils.extend.FDelayTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
@@ -18,6 +21,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v)
     {
-
+        mDelayTask.runDelayOrImmediately(1000);
     }
+
+    private final FDelayTask mDelayTask = new FDelayTask()
+    {
+        @Override
+        protected void onRun()
+        {
+            Log.i(TAG, "onRun");
+        }
+
+        @Override
+        protected void onPost()
+        {
+            super.onPost();
+            Log.i(TAG, "onPost");
+        }
+
+        @Override
+        protected void onRemove()
+        {
+            super.onRemove();
+            Log.i(TAG, "onRemove");
+        }
+    };
 }

46397a995136bce5f7dc780841f526f6bd52b035
zjun <565061763@qq.com>
2019-01-16 20:52:43 +0800

    demo

--- app/src/main/java/com/sd/utils_extend/MainActivity.java
+++ app/src/main/java/com/sd/utils_extend/MainActivity.java
@@ -1,94 +1,23 @@
 package com.sd.utils_extend;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 
-import com.sd.lib.utils.extend.FImageCompressor;
-import com.sd.lib.utils.extend.FImageGetter;
-
-import java.io.File;
-
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
     public static final String TAG = MainActivity.class.getSimpleName();
-
-    private ImageView iv_image;
-
-    private FImageGetter mImageGetter;
-    private FImageCompressor mImageCompressor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        iv_image = findViewById(R.id.iv_image);
-
-        testImageCompressor();
-        testImageGetter();
-
-        iv_image.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                mImageGetter.getImageFromAlbum();
-            }
-        });
-    }
-
-    private void testImageGetter()
-    {
-        mImageGetter = new FImageGetter(this);
-        mImageGetter.setCallback(new FImageGetter.Callback()
-        {
-            @Override
-            public void onResultFromAlbum(File file)
-            {
-                Log.i(TAG, "onResultFromAlbum:" + file.getAbsolutePath());
-
-                Bitmap bitmap = mImageCompressor.compressFileToBitmap(file.getAbsolutePath());
-                iv_image.setImageBitmap(bitmap);
-            }
-
-            @Override
-            public void onResultFromCamera(File file)
-            {
-                Log.i(TAG, "onResultFromCamera:" + file.getAbsolutePath());
-            }
-
-            @Override
-            public void onError(String desc)
-            {
-                Log.e(TAG, desc);
-            }
-        });
-    }
-
-    private void testImageCompressor()
-    {
-        mImageCompressor = new FImageCompressor(this);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    public void onClick(View v)
     {
-        super.onActivityResult(requestCode, resultCode, data);
-        mImageGetter.onActivityResult(requestCode, resultCode, data);
-    }
 
-    @Override
-    protected void onDestroy()
-    {
-        super.onDestroy();
-        if (mImageCompressor != null)
-        {
-            mImageCompressor.deleteAllCompressedFile();
-        }
     }
 }

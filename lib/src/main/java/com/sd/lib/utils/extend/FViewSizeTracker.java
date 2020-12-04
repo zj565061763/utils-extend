f4aeaae20561b22162812418106e175ec14645fb
zjun <565061763@qq.com>
2020-12-04 16:31:43 +0800

    FViewSizeTracker

---
+++ lib/src/main/java/com/sd/lib/utils/extend/FViewSizeTracker.java
@@ -0,0 +1,231 @@
+package com.sd.lib.utils.extend;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * View大小追踪，让源View追踪目标View的大小
+ */
+public class FViewSizeTracker
+{
+    private final BoundaryHandler mBoundaryHandler;
+
+    private WeakReference<View> mSource;
+    private WeakReference<View> mTarget;
+
+    public FViewSizeTracker()
+    {
+        this(Boundary.Both);
+    }
+
+    public FViewSizeTracker(Boundary boundary)
+    {
+        if (boundary == null)
+            throw new NullPointerException("boundary is null");
+
+        switch (boundary)
+        {
+            case Width:
+                mBoundaryHandler = new WidthHandler();
+                break;
+            case Height:
+                mBoundaryHandler = new HeightHandler();
+                break;
+            case Both:
+                mBoundaryHandler = new BothHandler();
+                break;
+            default:
+                throw new RuntimeException("unknown boundary:" + boundary);
+        }
+    }
+
+    public final View getSource()
+    {
+        return mSource == null ? null : mSource.get();
+    }
+
+    public final View getTarget()
+    {
+        return mTarget == null ? null : mTarget.get();
+    }
+
+    /**
+     * 设置源View
+     *
+     * @param source
+     */
+    public final void setSource(View source)
+    {
+        final View old = getSource();
+        if (old != source)
+        {
+            mSource = source == null ? null : new WeakReference<>(source);
+            checkSize();
+        }
+    }
+
+    /**
+     * 设置目标View
+     *
+     * @param target
+     */
+    public final void setTarget(View target)
+    {
+        final View old = getTarget();
+        if (old != target)
+        {
+            if (old != null)
+                old.removeOnLayoutChangeListener(mOnLayoutChangeListener);
+
+            mTarget = target == null ? null : new WeakReference<>(target);
+
+            if (target != null)
+                target.addOnLayoutChangeListener(mOnLayoutChangeListener);
+
+            checkSize();
+        }
+    }
+
+    private final View.OnLayoutChangeListener mOnLayoutChangeListener = new View.OnLayoutChangeListener()
+    {
+        @Override
+        public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom)
+        {
+            checkSize();
+        }
+    };
+
+    private void checkSize()
+    {
+        final View source = getSource();
+        if (source == null)
+            return;
+
+        final View target = getTarget();
+        if (target == null)
+            return;
+
+        if (!mBoundaryHandler.isTargetReady(target))
+            return;
+
+        if (mBoundaryHandler.isSizeEquals(target, source))
+            return;
+
+        final ViewGroup.LayoutParams layoutParams = source.getLayoutParams();
+        if (layoutParams == null)
+            return;
+
+        if (mBoundaryHandler.updateSourceLayoutParams(target, source, layoutParams))
+            source.setLayoutParams(layoutParams);
+    }
+
+    private static abstract class BoundaryHandler
+    {
+        public abstract boolean isTargetReady(View target);
+
+        public abstract boolean isSizeEquals(View target, View source);
+
+        public abstract boolean updateSourceLayoutParams(View target, View source, ViewGroup.LayoutParams layoutParams);
+    }
+
+    private static final class WidthHandler extends BoundaryHandler
+    {
+        @Override
+        public boolean isTargetReady(View target)
+        {
+            return target.getWidth() > 0;
+        }
+
+        @Override
+        public boolean isSizeEquals(View target, View source)
+        {
+            return target.getWidth() == source.getWidth();
+        }
+
+        @Override
+        public boolean updateSourceLayoutParams(View target, View source, ViewGroup.LayoutParams layoutParams)
+        {
+            final int width = target.getWidth();
+            if (layoutParams.width != width)
+            {
+                layoutParams.width = width;
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private static final class HeightHandler extends BoundaryHandler
+    {
+        @Override
+        public boolean isTargetReady(View target)
+        {
+            return target.getHeight() > 0;
+        }
+
+        @Override
+        public boolean isSizeEquals(View target, View source)
+        {
+            return target.getHeight() == source.getHeight();
+        }
+
+        @Override
+        public boolean updateSourceLayoutParams(View target, View source, ViewGroup.LayoutParams layoutParams)
+        {
+            final int height = target.getHeight();
+            if (layoutParams.height != height)
+            {
+                layoutParams.height = height;
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private static final class BothHandler extends BoundaryHandler
+    {
+        @Override
+        public boolean isTargetReady(View target)
+        {
+            return target.getWidth() > 0 && target.getHeight() > 0;
+        }
+
+        @Override
+        public boolean isSizeEquals(View target, View source)
+        {
+            return target.getWidth() == source.getWidth() &&
+                    target.getHeight() == source.getHeight();
+        }
+
+        @Override
+        public boolean updateSourceLayoutParams(View target, View source, ViewGroup.LayoutParams layoutParams)
+        {
+            boolean changed = false;
+
+            final int width = target.getWidth();
+            if (layoutParams.width != width)
+            {
+                layoutParams.width = width;
+                changed = true;
+            }
+
+            final int height = target.getHeight();
+            if (layoutParams.height != height)
+            {
+                layoutParams.height = height;
+                changed = true;
+            }
+
+            return changed;
+        }
+    }
+
+    public enum Boundary
+    {
+        Width,
+        Height,
+        Both
+    }
+}
